6869998579c9b2ab3f3ba0ee9260f64363b88de3
Garett Bass <garettbass@me.com>
2021-01-26 19:00:50 -0800

    publish

--- build/app-root.entry.js
+++ build/app-root.entry.js
@@ -6,6 +6,19 @@ const AppRoot = class {
   constructor(hostRef) {
     registerInstance(this, hostRef);
   }
+  connectedCallback() {
+    document.body.addEventListener('touchstart', (e) => {
+      var width = window.innerWidth;
+      var margin = width / 10;
+      var touch = e.touches[0];
+      if (touch) {
+        var x = touch.pageX;
+        if (x < margin || x > width - margin) {
+          e.preventDefault();
+        }
+      }
+    });
+  }
   render() {
     return (h("ion-app", null, h("ion-router", { useHash: false }, h("ion-route", { url: "/", component: "app-home" }), h("ion-route", { url: "/auth", component: "vt-auth" }), h("ion-route", { url: "/profile/:name", component: "app-profile" })), h("ion-nav", null)));
   }
